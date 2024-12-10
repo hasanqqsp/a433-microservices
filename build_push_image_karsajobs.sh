9960625f1d7d879a7c03bcc40ab31c672aaa9b0d
Hasan Ismail <hasanqqsp@gmail.com>
2024-12-10 14:58:16 +0700

    add ci pipeline

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Nama image yang akan dibuild
+IMAGE_NAME="karsajobs"
+
+# Versi image
+IMAGE_VERSION="latest"
+
+
+# Namespace GHCR
+GHCR_NAMESPACE="hasanqqsp"
+
+# Build image
+docker build -t $IMAGE_NAME:$IMAGE_VERSION .
+
+# Login ke GHCR
+echo $CR_PAT | docker login ghcr.io -u $GHCR_NAMESPACE --password-stdin
+
+# Tag image dengan repository GHCR
+docker tag $IMAGE_NAME:$IMAGE_VERSION ghcr.io/$GHCR_NAMESPACE/$IMAGE_NAME:$IMAGE_VERSION
+
+# Push image ke GHCR
+docker push ghcr.io/$GHCR_NAMESPACE/$IMAGE_NAME:$IMAGE_VERSION
